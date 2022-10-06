93f8f39a192cac12d88c1e38291dc659e8f8d920
Kevin Chiu <keavenchiu@gmail.com>
2022-10-05 22:56:18 -0400

    add CardView

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,25 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by kevin chiu on 2022-10-03.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
